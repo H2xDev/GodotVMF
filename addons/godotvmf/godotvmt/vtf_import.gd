070c3f9b08db7d7e2f1c1481d6b5151b15acb11a
Radik Khamatdinov <h2xdeveloper@gmail.com>
2025-10-28 00:58:36 +0400

    VTF: Add conversion to SRGB

--- addons/godotvmf/godotvmt/vtf_import.gd
+++ addons/godotvmf/godotvmt/vtf_import.gd
@@ -11,16 +11,27 @@ func _get_import_order(): return 0;
 func _get_priority(): return 1;
 func _can_import_threaded(): return false;
 
-func _get_import_options(str, int): return [];
+func _get_import_options(str, int): return [
+	{
+		name = "srgb_conversion_method",
+		default_value = 1,
+		type = TYPE_INT,
+		property_hint = PROPERTY_HINT_ENUM,
+		hint_string = "Disabled, During import, Process in shader",
+		description = "Perform conversion from SRGB to Linear"
+	}
+];
+
 func _get_option_visibility(path: String, optionName: StringName, options: Dictionary): return true;
 
-func _import(path: String, save_path: String, _a, _b, _c):
+func _import(path: String, save_path: String, options: Dictionary, _b, _c):
 	var path_to_save = save_path + '.' + _get_save_extension();
 	var vtf = VTFLoader.create(path, 0.033); # 30 FPS
 
 	if !vtf: return ERR_FILE_UNRECOGNIZED;
 
-	var texture = vtf.compile_texture();
+	var texture = vtf.compile_texture(options.srgb_conversion_method);
 	if not texture: return ERR_FILE_CORRUPT;
+	texture.set_meta("srgb_conversion_method", options.srgb_conversion_method);
 
 	return ResourceSaver.save(texture, path_to_save, ResourceSaver.FLAG_COMPRESS);
