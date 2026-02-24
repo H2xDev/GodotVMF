45f0ed04954d41e0ed010a36db86f3023d4a139f
Asrael <70919950+asraeldragon@users.noreply.github.com>
2026-02-24 09:25:13 -0800

    VMT: Add support for .res in loaded materials (#82)
    * Add support for .res in loaded materials
    
    * Added Asrael to contributors

--- addons/godotvmf/godotvmt/vmt_loader.gd
+++ addons/godotvmf/godotvmt/vmt_loader.gd
@@ -31,6 +31,14 @@ static func parse_transform(transform_data: String):
 		translate = translate,
 	}
 
+static func get_material_load_path(material: String) -> String:
+	var path_root = normalize_path(VMFConfig.materials.target_folder + "/" + material).to_lower();
+
+	if ResourceLoader.exists(path_root + ".tres"): return path_root + ".tres";
+	if ResourceLoader.exists(path_root + ".res"): return path_root + ".res";
+	if ResourceLoader.exists(path_root + ".vmt"): return path_root + ".vmt";
+	return "";
+
 static func load(path: String):
 	var structure = VDFParser.parse(path, true);
 
@@ -106,15 +114,7 @@ static func normalize_path(path: String) -> String:
 	return path.replace('\\', '/').replace('//', '/').replace('res:/', 'res://');
 
 static func has_material(material: String) -> bool:
-	var material_path = normalize_path(VMFConfig.materials.target_folder + "/" + material + ".tres").to_lower();
-
-	if not ResourceLoader.exists(material_path):
-		material_path = material_path.replace(".tres", ".vmt");
-
-	if not ResourceLoader.exists(material_path):
-		return false;
-
-	return true;
+	return get_material_load_path(material) != "";
 
 static func is_material_ignored(material_name: String) -> bool:
 	return VMFConfig.materials.ignore.any(func(rx: String) -> bool: return material_name.match(rx.to_lower()));
@@ -124,12 +124,8 @@ static func get_material(material: String) -> Material:
 	if cached_material:
 		return cached_material as Material;
 
-	var material_path = normalize_path(VMFConfig.materials.target_folder + "/" + material + ".tres").to_lower();
-
-	if not ResourceLoader.exists(material_path):
-		material_path = material_path.replace(".tres", ".vmt");
-
-	if not ResourceLoader.exists(material_path):
+	var material_path = get_material_load_path(material);
+	if material_path == "":
 		if not VMFCache.is_file_logged(material_path):
 			VMFLogger.warn("Material not found: " + material_path);
 			VMFCache.add_logged_file(material_path);
